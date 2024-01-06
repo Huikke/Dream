21f6abb34b3681355721d5dbac80ac8527de060e
Lassi Chen <lachen@utu.fi>
2024-01-06 22:44:25 +0200

    changed movement to have acceleration

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-var speed = 500
+var speed = 10
 var size = 50
 
 func _process(delta):
 	# Input
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed * delta
+	velocity += direction * speed
+	move_and_slide()
 
 func _draw():
 	draw_circle(Vector2(0,0),size,Color(1,1,1))
